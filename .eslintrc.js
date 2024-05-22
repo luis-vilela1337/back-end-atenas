1f89cb50076b855ac2b73132ac3ff3aa81d703aa
LuisFillype <l.fillype@gmail.com>
2024-05-22 18:48:18 -0300

    chore: atualizar arquivos de config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir : __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -24,14 +24,10 @@ module.exports = {
     '@typescript-eslint/no-namespace': 'off',
     "@typescript-eslint/naming-convention": [
       "error",
-        {
-          "selector": "interface",
-          "format": ["PascalCase"],
-          "custom": {
-            "regex": "^I[A-Z]",
-            "match": true
-          }
-        }
-      ],
+      {
+        "selector": "interface",
+        "format": ["PascalCase"],
+      }
+    ],
   },
 };
